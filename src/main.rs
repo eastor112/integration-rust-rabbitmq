ae8841463eb768cd1089d4398c332a71b90253b3
Emerson Asto <emerar.mct@gmail.com>
2025-06-06 16:39:23 -0500

    Change server port from 8080 to 8081 in main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(send_notification)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await
 }
